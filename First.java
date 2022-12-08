9873ac72fd73f610c40c77c1727a899c76560bf9
mkrtwr51 <66071794+mkrtwr51@users.noreply.github.com>
2022-12-08 16:48:26 +0530

    Update First.java

--- First.java
+++ First.java
@@ -3,5 +3,7 @@ public class First{
     public static void main(String[] args) {
         System.out.print("heloofrom vs ");
         System.out.println("second commit");
+         System.out.println("from git commit");
+        
     }
 }
